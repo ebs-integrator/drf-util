7c9515b2406ab93416a69ba0b7e8aee71cfc1356
Nicolae Godina <nicolae.godina@ebs-integrator.com>
2019-08-03 12:34:24 +0300

    up version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='drf_util',
-    version='0.0.13',
+    version='0.0.14',
     description='Django Rest Framework Utils',
     long_description=long_description,
     long_description_content_type="text/markdown",
