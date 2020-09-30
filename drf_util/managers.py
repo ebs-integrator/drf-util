9f23f633491f4447c8f0abe919743a0c5a297508
artiom <ordersone@gmail.com>
2020-09-30 10:35:27 +0300

    Override delete method on NoDeleteModel queryset

--- drf_util/managers.py
+++ drf_util/managers.py
@@ -1,6 +1,14 @@
+from django.utils import timezone
 from django.db import models
+from django.db.models.manager import BaseManager
 
 
-class NoDeleteManager(models.Manager):
+class NoDeleteQuerySet(models.query.QuerySet):
+    def delete(self):
+        self.update(date_deleted=timezone.now())
+        return self.count(), None
+
+
+class NoDeleteManager(BaseManager.from_queryset(NoDeleteQuerySet)):
     def get_queryset(self):
-        return super(NoDeleteManager, self).get_queryset().filter(date_deleted__isnull=True)
+        return super().get_queryset().filter(date_deleted__isnull=True)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
-
 from drf_util import models
+
+from tests.models import Thing
 
 
 class ModelsTests(TestCase):
+    def setUp(self):
+        print('All objects count', Thing.all_objects.count())
 
     def test_get_default_languages(self):
         data = {
@@ -15,3 +18,29 @@ class ModelsTests(TestCase):
         self.assertEqual(models.get_lang_value(data), 'english')
         self.assertEqual(models.get_lang_value(data, 'ro'), 'romanian')
         self.assertIsNone(models.get_lang_value(data, 'ru'))
+
+    def test_no_delete_model(self):
+        first_thing = Thing.objects.create(title='First')
+
+        self.assertEqual(None, first_thing.date_deleted)
+
+        first_thing.delete()
+
+        self.assertNotEqual(None, first_thing)
+        self.assertNotEqual(None, first_thing.date_deleted)
+
+        second_thing = Thing.objects.create(title='Second')
+
+        self.assertEqual(None, second_thing.date_deleted)
+
+        Thing.objects.all().delete()
+        second_thing.refresh_from_db()
+
+        self.assertNotEqual(None, second_thing)
+        self.assertNotEqual(None, second_thing.date_deleted)
+
+        self.assertEqual(0, Thing.objects.count())
+        self.assertEqual(2, Thing.all_objects.count())
+
+        # Check if test is running
+        # self.assertEqual(True, False)
